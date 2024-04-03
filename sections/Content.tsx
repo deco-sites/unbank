617f9ccc3dbfbeb1f510ce7527109e6a2dd24afa
igorbrands <igorcantelmo@gmail.com>
2024-04-03 09:35:30 -0300

    feat: create content section

---
+++ sections/Content.tsx
@@ -0,0 +1,82 @@
+export default function Content() {
+  return (
+    <section>
+      <div>
+        <h2>
+          Tudo mudou para continuar da mesma forma como estava.
+        </h2>
+      </div>
+
+      <div>
+        <p>
+          A revolução dos neobanks na última década mudou completamente o
+          cenário bancário no Brasil. No caminho, gerou uma enorme riqueza. O
+          neobank mais valioso do mundo nasceu aqui.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          Porém, do ponto de vista do modelo de negócios, muito pouco mudou. O
+          modelo de negócio de um neobank e de um bancão é igual. E a riqueza
+          criada foi, em sua imensa maioria, para a mão dos investidores,
+          fundadores e funcionários.
+        </p>
+        <p>
+          Bancos são negócios construídos em cima de uma rede de confiança. Os
+          clientes foram essenciais para a construção dessa confiança, desde o
+          início.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          Os clientes trabalharam de graça porque queriam uma alternativa à
+          experiência terrível que tinham com os 5 bancões. Promoveram a marca e
+          gastaram tempo na defesa dela nas redes sociais. Trouxeram os amigos e
+          venceram a desconfiança de testar uma nova experiência.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>E o que sobrou para os clientes?</p>
+        <p>
+          <br />
+        </p>
+        <p>
+          A experiência bancária melhorou muito, se digitalizou, e a população
+          foi bancarizada. Sem dúvida: hoje o brasileiro médio tem mais de 4
+          contas bancárias. As tarifas para manutenção de conta realmente
+          caíram. E os bancões mostraram que também sabem fazer uma experiência
+          digital.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          Mas o que foi proposto como a missão dos disruptores – reduzir as
+          taxas de juros e spreads bancários, criar um banco onde o cliente está
+          no centro e não há conflito de interesse - falhou e foi
+          progressivamente saindo do discurso.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          Hoje as taxas de juros dos bancões são até menores do que as
+          praticadas pelos neobanks. O Brasil continua sendo um dos países com a
+          experiência bancária mais caras do mundo. E vemos uma série de
+          instâncias onde, quando o interesse dos acionistas do banco se
+          confronta com os clientes, os clientes saem perdendo.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          Tínhamos 5 grandes bancos, muito parecidos. Hoje temos 6, também muito
+          parecidos. Só mudam as cores.
+        </p>
+      </div>
+    </section>
+  );
+}
